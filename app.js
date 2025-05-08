31e9660c654be520b14e7d3b6f445cffbb8eff6f
carlos-lucenag <carlos.henriche.goncalves@ccc.ufcg.edu.br>
2025-05-08 12:24:36 -0300

    Conectando com banco de dados postgresql (ok)

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const pool = require("./backend/db");
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
-})
+  console.log(`Example app listening on port ${port}`);
+});
+
+app.get("/test-db", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT NOW()");
+    res.send(result.rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error while connecting to database");
+  }
+});
