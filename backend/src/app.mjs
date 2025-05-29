e74a73a8231982ec1af1a74024add292a6d419df
carlos-lucenag <carlos.henriche.goncalves@ccc.ufcg.edu.br>
2025-05-28 21:27:41 -0300

    Starting Frontend

--- backend/src/app.mjs
+++ backend/src/app.mjs
@@ -7,12 +7,25 @@ import cors from "cors";
 const app = express();
 
 // Middlewares
+app.use(cors());
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "POST, PUT, DELETE, OPTIONS, GET");
+  next();
+});
+
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 // Routes
 app.use("/api/clients", router);
 
-app.use(cors());
 
 app.get("/", (req, res) => {
   res.status(200).json({ message: "Hello World!" });
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,31 @@
-import React, { useEffect, useState } from 'react'
-import axios from 'axios'
+import { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
-  const [message, setMessage] = useState('');
+  const [clients, setClients] = useState([]);
 
   useEffect(() => {
-    axios.get('http://localhost:3000/')
-    .then(res => setMessage(res.data.message))
-    .catch(err => console.error(err));
+    fetchClients();
   }, []);
 
-  return (
-    <div>
-      <h1>React + Express</h1>
-      <p>{message}</p>
-    </div>
-  )
+  const fetchClients = async () => {
+    const response = await fetch("http://localhost:3000/api/clients");
+    const data = await response.json();
+    setClients(data);
+  };
 
+  return (
+    <div className="App">
+      <h1>Clients</h1>
+      {clients.map((client) => (
+        <div key={client.id}>
+          <ol>
+            {client.first_name}, {client.last_name}, {client.email}, {client.phone}, {client.city}
+          </ol>
+        </div>
+      ))}
+    </div>
+  );
 }
 
-export default App
+export default App;
